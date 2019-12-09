20b682ada93d1641499b0f5747c3b9e512ba8a11
keiichi <ohnary@tacoture.com>
2019-12-09 17:33:18 +0900

    commit

--- src/Controller/SongsController.php
+++ src/Controller/SongsController.php
@@ -2,25 +2,41 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Datasource\Exception\RecordNotFoundException;
 
 class SongsController extends AppController {
 
     public function initialize()
     {
-        // parent::initialize();
-        // $this->autoRender = false;
-        // $this->response->charset('UTF-8');
-        // $this->response->type('json');
+        parent::initialize();
+        $this->autoRender = false;
+        $this->response->charset('UTF-8');
+        $this->response->type('json');
     }
 
-    public function index(string $id = null)
+    public function index()
     {
-        $song = $this->Songs->find();
-        // $song = null;
-        if (empty($song)) {
-            $song = ['member' => null];
+        $songs = $this->Songs->find();
+        if (empty($songs)) {
+            $songs = ['member' => null];
         }
-        $body = json_encode($song);
+        $body = json_encode($songs);
         $this->response->body($body);
+    }
+
+    public function get(?string $id = null)
+    {
+        if (!ctype_digit($id)) {
+            $song = null;
+        } else {
+            try {
+                $song = $this->Songs->get($id);
+            } catch (RecordNotFoundException $e) {
+                $song = null;
+            }
+        }
+        $this->response->body(json_encode([
+            'song' => $song
+        ]));
     }
 }
